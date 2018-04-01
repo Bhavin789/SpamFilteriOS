152da312cc3810e2dcd47b87958fed94eea2dfc4
Bhavin789 <bhavib.789@gmail.com>
2018-04-01 18:54:29 +0530

    Includes the tfidf function

--- SpamFilteriOSApp/SpamFilteriOSApp/ViewController.swift
+++ SpamFilteriOSApp/SpamFilteriOSApp/ViewController.swift
@@ -20,6 +20,25 @@ class ViewController: MSMessagesAppViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    @IBAction func pushed(_ sender: Any){
+        
+    }
+    
+    func tfidf(sms: String) -> MLMultiArray{
+        let wordsFile = Bundle.main.path(forResource: "words_ordered", ofType: "txt")
+        let smsFile = Bundle.main.path(forResource: "SMSSpamCollection", ofType: "txt")
+        do {
+            let wordsFileText = try String(contentsOfFile: wordsFile!, encoding: String.Encoding.utf8)
+            var wordsData = wordsFileText.components(separatedBy: .newlines)
+            wordsData.removeLast() // Trailing newline.
+            let smsFileText = try String(contentsOfFile: smsFile!, encoding: String.Encoding.utf8)
+            var smsData = smsFileText.components(separatedBy: .newlines)
+            smsData.removeLast() // Trailing newline.
+            let wordsInMessage = sms.split(separator: " ")
+            var vectorized = try MLMultiArray(shape: [NSNumber(integerLiteral: wordsData.count)], dataType: MLMultiArrayDataType.double)
+            
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
